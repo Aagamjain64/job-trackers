8e087cafee0083fc13843387f67a6554a88cf319
Aagam Jain <aagamjain0604@gmail.com>
2026-01-17 15:40:38 +0530

    Fix CORS origin URL by removing trailing slash

--- backend/main.js
+++ backend/main.js
@@ -11,7 +11,7 @@
     app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:5173","https://job-trackerss.netlify.app/"], // frontend
+  origin: ["http://localhost:5173","https://job-trackerss.netlify.app"], // frontend
   credentials: true
 }));
     // Routes
